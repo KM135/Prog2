4175d75d137a954636d33a6463940a2df5389a0f
JulioAlberto <JulioAlberto@OverWatch>
2018-05-10 11:46:28 -0500

    Subo clases mecanico reparacion y vehiculo

--- src/modelo/Vehiculo.java
+++ src/modelo/Vehiculo.java
@@ -1,5 +1,30 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Vehiculo {
+	private String placa;
+	private ArrayList<Reparacion> reparaciones;
+
+	public Vehiculo(String placa) {
+		super();
+		this.placa = placa;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public ArrayList<Reparacion> getReparaciones() {
+		return reparaciones;
+	}
+
+	public void setReparaciones(ArrayList<Reparacion> reparaciones) {
+		this.reparaciones = reparaciones;
+	}
 
 }
