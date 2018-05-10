62f4edfb0e789f3081ebd8968ba9b473e9620b5c
Camilo <Camilo@Camilo-Cruz>
2018-05-10 11:52:59 -0500

    Creación de getters y setters clases Propietario y Taller... Posdta: Este comentario es mas largo debido al comentario de mi compañero, no le gusto la letra P como comentario.

--- src/modelo/Propietario.java
+++ src/modelo/Propietario.java
@@ -1,5 +1,63 @@
 package modelo;
 
-public class Propietario {
+import java.util.ArrayList;
 
+public class Propietario {
+	private int cedula;
+	private String nombre;
+	private String direccion;
+	private String telefono;
+	
+	private ArrayList <Vehiculo>carros;
+
+	public Propietario(int cedula, String nombre, String direccion, String telefono) {
+		super();
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		carros = new ArrayList <Vehiculo>();
+	}
+
+	public int getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(int cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public ArrayList<Vehiculo> getCarros() {
+		return carros;
+	}
+
+	public void setCarros(ArrayList<Vehiculo> carros) {
+		this.carros = carros;
+	}
+
+	
 }
--- src/modelo/Taller.java
+++ src/modelo/Taller.java
@@ -1,5 +1,54 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Taller {
+
+	private int id;
+	private String nombre;
+	private String direccion;
+	private int telefono;
+	private ArrayList <Mecanico>trabajadores;
+	
+
+	public Taller(int id, String nombre, String direccion, int telefono) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		trabajadores = new ArrayList <Mecanico>();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public ArrayList<Mecanico> getTrabajadores() {
+		return trabajadores;
+	}
+	public void setTrabajadores(ArrayList<Mecanico> trabajadores) {
+		this.trabajadores = trabajadores;
+	}
+	
 
 }
