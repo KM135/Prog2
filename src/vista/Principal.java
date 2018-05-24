20e1310c1d8b42c73494507caaab11a05b14c4e8
JulioAlberto <JulioAlberto@OverWatch>
2018-05-24 10:18:38 -0500

    Mas ajustes en la ventana principal

--- src/vista/Principal.java
+++ src/vista/Principal.java
@@ -13,6 +13,7 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
@@ -24,9 +25,7 @@ import modelo.Cliente;
 import modelo.Modelo;
 
 public class Principal extends JFrame implements ActionListener {
-	/**
-	 * 
-	 */
+
 	public final String PORTADA = "PORTADA";
 	public final String TALLER = "TALLER";
 	public final String CHAT = "CHAT";
@@ -35,36 +34,32 @@ public class Principal extends JFrame implements ActionListener {
 	private JButton portada, taller, chat, salir;
 	private Modelo modelo;
 
-	ImageIcon imagen;
-	Icon icono;
-	Font fuente = new Font("Arial", Font.ITALIC, 50);
-	Font fuente1 = new Font("Tahoma", Font.BOLD, 50);
-	Font fuente2 = new Font("Times New Roman", Font.PLAIN, 50);
+	Font fuenteTaller = new Font("Times New Roman", Font.LAYOUT_RIGHT_TO_LEFT, 63);
+	Font fuenteChat = new Font("Tahoma", Font.ITALIC, 35);
+	Font fuentePortada = new Font("Tahoma", Font.PLAIN, 30);
+	Font fuenteSalir = new Font("Arial", Font.ITALIC, 20);
 
 	public Principal() {
 
 		setSize(600, 300);
-		setTitle("Ventana de inicio");
-		setBackground(Color.WHITE);
 		setLocationRelativeTo(null);
-		// setLayout(new GridLayout(2, 2, 20, 20));
 		setLayout(new BorderLayout());
 		setUndecorated(true);
-
+		
 		portada = new JButton("Portada");
 		portada.setActionCommand(PORTADA);
 		portada.addActionListener(this);
 		portada.setForeground(new Color(17, 80, 70));
 		portada.setBackground(new Color(232, 234, 243));
-		portada.setFont(fuente2);
+		portada.setFont(fuentePortada);
 		add(portada, BorderLayout.NORTH);
 
-		taller = new JButton("Taller Woodcod");
+		taller = new JButton("Taller Woodcod ");
 		taller.setActionCommand(TALLER);
 		taller.addActionListener(this);
 		taller.setForeground(new Color(17, 80, 70));
 		taller.setBackground(Color.WHITE);
-		taller.setFont(fuente1);
+		taller.setFont(fuenteTaller);
 		add(taller, BorderLayout.CENTER);
 
 		chat = new JButton("Chat");
@@ -72,15 +67,15 @@ public class Principal extends JFrame implements ActionListener {
 		chat.addActionListener(this);
 		chat.setForeground(Color.WHITE);
 		chat.setBackground(new Color(54, 98, 236));
-		chat.setFont(fuente);
+		chat.setFont(fuenteChat);
 		add(chat, BorderLayout.WEST);
 
-		salir = new JButton("Salir");
+		salir = new JButton("                                                                             Salir");
 		salir.setActionCommand(SALIR);
 		salir.addActionListener(this);
 		salir.setForeground(Color.WHITE);
 		salir.setBackground(new Color(255, 0, 43));
-		salir.setFont(fuente2);
+		salir.setFont(fuenteSalir);
 		add(salir, BorderLayout.SOUTH);
 
 		setVisible(true);
