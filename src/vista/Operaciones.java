690cc70ba2d3840936847c742338836f52588e27
JulioAlberto <JulioAlberto@OverWatch>
2018-05-24 10:37:28 -0500

    Ajustes en botones

--- src/vista/Operaciones.java
+++ src/vista/Operaciones.java
@@ -1,5 +1,7 @@
 package vista;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
@@ -10,6 +12,7 @@ public class Operaciones extends JPanel{
 	
 	private JButton reparaciones, historialXAuto, autosregistrados; 
 	private VentanaTaller ventana;
+	Font fuente = new Font("Arial", Font.BOLD, 15);
 	
 	public Operaciones(VentanaTaller pVentana){
 		ventana= pVentana;
@@ -19,16 +22,25 @@ public class Operaciones extends JPanel{
 		reparaciones=new JButton("Listar Reparaciones");
 		reparaciones.setActionCommand(ventana.LISTAREPARACIONES);
 		reparaciones.addActionListener(ventana);
+		reparaciones.setForeground(new Color(232, 244, 233));
+		reparaciones.setBackground(new Color(20, 25, 81));
+		reparaciones.setFont(fuente);
 		add(reparaciones);
 		
 		historialXAuto=new JButton("Historial por vehiculo");
 		historialXAuto.setActionCommand(ventana.HISTXAUTO);
 		historialXAuto.addActionListener(ventana);
+		historialXAuto.setForeground(new Color(232, 244, 233));
+		historialXAuto.setBackground(new Color(20, 25, 81));
+		historialXAuto.setFont(fuente);
 		add(historialXAuto);
 		
 		autosregistrados=new JButton("Lista de vehiculos y propietarios");
 		autosregistrados.setActionCommand(ventana.AUTOSYDUEÑO);
 		autosregistrados.addActionListener(ventana);
+		autosregistrados.setForeground(new Color(232, 244, 233));
+		autosregistrados.setBackground(new Color(20, 25, 81));
+		autosregistrados.setFont(fuente);
 		add(autosregistrados);
 	}
 }
--- src/vista/Parametros.java
+++ src/vista/Parametros.java
@@ -1,5 +1,7 @@
 package vista;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
@@ -10,6 +12,7 @@ public class Parametros extends JPanel{
 	
 	private JButton cliente, auto, mecanico, reparacion, agregarE; 
 	private VentanaTaller taller;
+	Font fuente = new Font("Arial", Font.BOLD, 13);
 	
 	public Parametros(VentanaTaller taller){
 		this.taller=taller;
@@ -19,21 +22,33 @@ public class Parametros extends JPanel{
 		cliente=new JButton("Crear cliente");
 		cliente.setActionCommand(taller.CLIENTE);
 		cliente.addActionListener(taller);
+		cliente.setForeground(new Color(232, 244, 233));
+		cliente.setBackground(new Color(20, 25, 81));
+		cliente.setFont(fuente);
 		add(cliente);
 		
 		auto=new JButton("Asignar auto");
 		auto.setActionCommand(taller.AUTO);
 		auto.addActionListener(taller);
+		auto.addActionListener(taller);
+		auto.setForeground(new Color(232, 244, 233));
+		auto.setBackground(new Color(37, 47, 165));
 		add(auto);
 		
 		mecanico=new JButton("Crear mecanico");
 		mecanico.setActionCommand(taller.MECANICO);
 		mecanico.addActionListener(taller);
+		mecanico.setForeground(new Color(232, 244, 233));
+		mecanico.setBackground(new Color(130, 77, 0));
+		mecanico.setFont(fuente);
 		add(mecanico);
 		
 		reparacion=new JButton("Registrar orden");
 		reparacion.setActionCommand(taller.REPARACION);
 		reparacion.addActionListener(taller);
+		reparacion.setForeground(new Color(232, 244, 233));
+		reparacion.setBackground(new Color(209, 141, 81));
+		reparacion.setFont(fuente);
 		add(reparacion);
 	}
 }
